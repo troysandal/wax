f6b0b2788e1ebf2bec4138aa1755a0a52e218a68
Troy Sandal <troysandal@gmail.com>
2025-08-20 20:59:06 -0700

    updated for iOS 13+

--- tools/Tests/scripts/AppDelegate.lua
+++ tools/Tests/scripts/AppDelegate.lua
@@ -1,22 +1,29 @@
 waxClass{"AppDelegate", protocols = {"UIApplicationDelegate"}}
 
 function applicationDidFinishLaunching(self, application)
-  -- local frame = UIScreen:mainScreen():bounds()
-  -- self.window = UIWindow:initWithFrame(frame)
-  -- self.window:setBackgroundColor(UIColor:orangeColor())
-  -- 
-  -- local view = UIView:init()
-  -- 
-  -- local button = UIButton:buttonWithType(UIButtonTypeRoundedRect)
-  -- button:setTitle_forState("Test GC", UIControlStateNormal)
-  -- button:setFrame(CGRect(100, 100, 120, 100))
-  -- button:addTarget_action_forControlEvents(self, "gcTouched:", UIControlEventTouchUpInside)
-  -- self.window:addSubview(button)
-  
-  -- self.window:makeKeyAndVisible()
+  local frame = UIScreen:mainScreen():bounds()
+  self.window = UIWindow:initWithFrame(frame)
+  self.window:setBackgroundColor(UIColor:orangeColor())
+
+  -- Required as window root since iOS 13
+  local rootVC = UIViewController:init()
+
+  local view = UIView:initWithFrame(frame)
+  local button = UIButton:buttonWithType(UIButtonTypeRoundedRect)
+  button:setTitle_forState("Test GC", UIControlStateNormal)
+  button:setFrame(CGRect(0, 0, 120, 100))
+  button:addTarget_action_forControlEvents(self, "gcTouched:", UIControlEventTouchUpInside)
+  view:addSubview(button)
+
+  local rvcView = rootVC:view()
+  rvcView:addSubview(view)
+
+  self.window:setRootViewController(rootVC)
+  self.window:makeKeyAndVisible()
+  return true
 end
 
--- function gcTouched(self, sender)
---   puts("boom")
---   collectgarbage("collect")
--- end
+function gcTouched(self, sender)
+  puts("boom")
+  collectgarbage("collect")
+end
